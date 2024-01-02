30c8d0f4a5d1b7dade7ee2be304f0fed291dc1e1
itsyous <youssef.rabih1995@gmail.com>
2024-01-02 04:19:27 -0800

    copy paste

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 word = "Holberton"
 word_first_3 = "wor"
-word_last_2 = "wo"
+word_last_2 = "rd"
 middle_word = "or"
 print(f"First 3 letters: {word_first_3}")
 print(f"Last 2 letters: {word_last_2}")
