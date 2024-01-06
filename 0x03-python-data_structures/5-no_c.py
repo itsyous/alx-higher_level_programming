45722a9e8bb71d169d91272af71fe759bd524a16
itsyous <youssef.rabih1995@gmail.com>
2024-01-06 15:54:39 -0800

    no c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,4 +4,4 @@ def no_c(my_string):
         new_string = my_string.translate({ord("c"): None})
         sec_string = new_string.translate({ord("C"): None})
         return sec_string
-    return new_string
+    return my_string
