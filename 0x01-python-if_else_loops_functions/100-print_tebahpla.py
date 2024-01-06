3c543f4889cf4bfd7b5f67a46b9b28ae4bcd5373
itsyous <youssef.rabih1995@gmail.com>
2024-01-06 14:29:33 -0800

    rev alph

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
-i=122
-while i >= 97:
-    if i % 2 != 0:
-        i = i - 32
-        print(chr(i))
-    else:
-        print(chr(i))
-    i = i - 1
+for i in range(ord('z'), ord('a'), -1, -2):
+    print("{}{}".format(chr(i), chr(i - 33)), end='')
