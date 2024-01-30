09765b97bc2f10b0ebd51745ac2515c095350594
itsyous <youssef.rabih1995@gmail.com>
2024-01-30 15:18:21 -0800

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""define a class"""
+
+class square:
+    """initialize the square
+
+    Args:
+    size: lenght of a side of square
+    """
+    def __init__(self, size):
+        self.__size = size
