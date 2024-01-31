b1145716c5e26cbc328cc08763a93a632119f67e
itsyous <youssef.rabih1995@gmail.com>
2024-01-31 09:06:38 -0800

    square-3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
-"""define a class"""
+"""Define a class Square"""
 
 class Square:
-    """Represent a square"""
+    """Represent a square."""
 
     def __init__(self, size=0):
-        """initialize a square.
+        """Constructor.
 
         Args:
-        size: lenght of a side of the square
+        Size: lenght of a side of the square
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        if size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
 
         def area(self):
-            """return a squared size"""
+            """Return a squared size"""
             return (self.__size ** 2)
