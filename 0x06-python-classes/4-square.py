dd5567b2d7f31f6e96d56b7a3e50d9197f095f9f
itsyous <youssef.rabih1995@gmail.com>
2024-01-31 12:40:17 -0800

    square-4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -17,13 +17,15 @@ class Square:
     def size(self):
         """retrieve the size of square"""
         return self.__size
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif size < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
     def area(self):
         """return the squared size"""
         return (self.__size ** 2)
