c50a1066c9ac4d6c4e57731cc9731ef1b6736802
itsyous <youssef.rabih1995@gmail.com>
2024-01-31 07:45:58 -0800

    square-2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""define a square"""
+
+class Square:
+    """Represent a square"""
+
+    def __init__(self, size = 0):
+        """initialize a square
+
+        Args:
+        size: lenght of a side of square
+        """
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise TypeError('size must be >= 0')
+        self.__size = size
+
