c511ee476fdc5df03384e24149b375bdf2195d07
itsyous <youssef.rabih1995@gmail.com>
2024-01-31 10:07:23 -0800

    square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Empty class square"""
 
+
 class Square:
     """class square that represent the square"""
     pass
--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """define a square"""
 
+
 class Square:
     """Represent a square"""
 
