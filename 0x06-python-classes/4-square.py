5a370a019daefd0aa09082166c96334ae213dab6
itsyous <youssef.rabih1995@gmail.com>
2024-01-31 12:37:09 -0800

    square-4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""define a class"""
+
+
+class Square:
+    """Represent a square"""
+
+    def __init__(self, size=0):
+        """initialize a square
+
+        Args:
+        size: the lenght of a side of square
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """retrieve the size of square"""
+        return self.__size
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+    def area(self):
+        """return the squared size"""
+        return (self.__size ** 2)
