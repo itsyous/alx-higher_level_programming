a39e4dd82642ea54f840d300c30883c80e703cd9
itsyous <youssef.rabih1995@gmail.com>
2024-01-31 08:10:45 -0800

    square-3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""define a class"""
+
+class Square:
+    """Represent a square"""
+
+    def __init__(self, size = 0):
+        """initialize a square
+
+        Args:
+        size: lenght of a side of the square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise TypeError("size must be >= 0")
+        self.__size = size
+
+        def area(self):
+            """define a method"""
+            return (self.__size ** 2)
