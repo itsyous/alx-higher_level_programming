16a772a98d62b1ed8c6c2302db513b9abcddcf74
itsyous <youssef.rabih1995@gmail.com>
2024-02-05 07:54:14 -0800

    My integer

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""contains class Myint"""
+
+
+class Myint(int):
+    """rebel version of an integer"""
+    def __new__(cls, *args, **kwargs):
+        """create an instance of class"""
+        return super(Myint, cls).__new__(cls, *args, **kwargs)
+
+    def __eq__(self, other):
+        """what was != is =="""
+        return int(self) != other
+
+    def __ne__(self, other):
+        """what was == is !="""
+        return int(self) == other
