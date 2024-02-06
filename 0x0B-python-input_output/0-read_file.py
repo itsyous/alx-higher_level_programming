ef229f11a3291e6cc6bda6dad724f00f2b54b0ad
itsyous <youssef.rabih1995@gmail.com>
2024-02-06 07:15:19 -0800

    read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""define read file finction"""
+
+
+def read_file(filename=""):
+    """read filename"""
+    with open(filename, encoding='utf-8' as f:
+            print(f.read(), end="")
