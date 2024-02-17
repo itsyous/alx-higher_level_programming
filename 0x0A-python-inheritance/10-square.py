e6593fcf0bf46286144926724e7e2a5b9cffb907
itsyous <youssef.rabih1995@gmail.com>
2024-02-17 11:24:38 -0800

    Square #1

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""module for Rectangle"""
+Rectangle = __import__('9-rectangle').BaseGeometry
+
+
+class Square(Rectangle):
+    """represent a class"""
+
+    def __init__(self, size):
+        """initialize a class
+
+        Args:
+        size (int): the size of the square
+        """
+
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
