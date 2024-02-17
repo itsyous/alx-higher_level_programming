edb096ad5ce5bfb4621a5ef85b66d05b01a19e29
itsyous <youssef.rabih1995@gmail.com>
2024-02-17 15:41:15 -0800

    Coordinates of a square

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""define a class"""
+
+
+class Square:
+    """Represent a square"""
+
+    def __init__(self, size=0):
+        """initialize a square
+
+        Args:
+        size: the lenght of a side of square
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """retrieve the size of square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """return the squared size"""
+        return (self.__size ** 2)
+
+    def my_print(self):
+        """print the square with '#'"""
+        for i in range(0, self.__size):
+            [print("#", end="") for j in range(self.__size)]
+            print("")
+        if self.__size == 0:
+            print("")
+            return
+
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(""), end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            print("")
