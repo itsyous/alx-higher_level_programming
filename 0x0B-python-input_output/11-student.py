aed70e0a2cee15c5072840155afac64b52cd683f
itsyous <youssef.rabih1995@gmail.com>
2024-02-28 15:44:45 -0800

    task 11

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -30,5 +30,5 @@ class Student:
         Args:
          json (dict): the key/value pairs to replace attributes with
         """
-         for i, j in json.items():
-             setattr(self, i, j)
+         for k, v in json.items():
+             setattr(self, k, v)
