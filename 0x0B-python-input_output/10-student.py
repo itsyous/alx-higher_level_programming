cd6ed042c583ab9500083595e025b8d19e899263
itsyous <youssef.rabih1995@gmail.com>
2024-02-28 15:28:19 -0800

    task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""containing class"""
+
+
+class Student:
+    """represent a class"""
+    def __init__(self, first_name, last_name, age):
+        """initialize a class"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """get a dictionary representation of the student
+
+        If attrs is a list of strings,only attribute names
+        contained in this list must be retrieved.
+
+        Args:
+          attrs (lis): the attributtes to represent
+        """
+        if (type(attrs) is list and
+           all(type(i) is str for i in attrs)):
+            return {j: getattr(self, j) for j in attrs if hasattr(self, j)}
+        return self.__dict__
