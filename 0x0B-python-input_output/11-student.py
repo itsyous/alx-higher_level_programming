5f220c610192db4226ed0ac192562ec22a968195
itsyous <youssef.rabih1995@gmail.com>
2024-02-28 15:35:07 -0800

    task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""containing class"""
+
+
+class Student:
+    """represent a class"""
+    def __init__(self, first_name, last_name, age):
+        """initialize a class"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """get a dictionary representation of the student
+
+        If attrs is a list of strings,only attribute names
+        contained in this list must be retrieved.
+
+        Args:
+          attrs (lis): the attributtes to represent
+        """
+        if (type(attrs) is list and
+           all(type(i) is str for i in attrs)):
+            return {j: getattr(self, j) for j in attrs if hasattr(self, j)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """replace all attributtes of the student
+
+        Args:
+         json (dict): the key/value pairs to replace attributes with
+         """
+         for j, i in jso,.items():
+             setattr(self, i, j)
