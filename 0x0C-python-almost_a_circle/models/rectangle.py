a6cb9ac3d1bd56ff34c772c70fed281a36366dfe
itsyous <youssef.rabih1995@gmail.com>
2024-03-04 12:26:56 -0800

    update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -7,7 +7,7 @@ class Rectangle(Base):
     """Represent a class"""
 
 
-    def __init__(self, width, height, x=0, y=0, id=None)
+    def __init__(self, width, height, x=0, y=0, id=None):
         """Initialize a class
 
         Args:
