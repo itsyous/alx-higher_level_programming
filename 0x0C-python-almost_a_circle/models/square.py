0d8813d1e95b57581153f04b4d5e8cbdb9f4d835
itsyous <youssef.rabih1995@gmail.com>
2024-03-06 03:35:47 -0800

    square

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""define a square class"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """represent a class"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initialize a class
+
+        Args:
+         size: the size of the square
+         x: the x coordinate of the square
+         y: the y coordinate of the square
+         id (int): the id if the square
+         """
+         super().__init__(size, size, x, y, id)
+
