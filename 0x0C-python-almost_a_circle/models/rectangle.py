217defad1954ea7b60e4394f432276af154ad25d
itsyous <youssef.rabih1995@gmail.com>
2024-03-10 16:57:23 -0700

    update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -98,21 +98,17 @@ class Rectangle(Base):
                 self.y = arg
             x += 1
 
-        if kwargs is not None:
-            for key, value in kwargs.items():
-                if key == "id":
-                    if value is None:
-                        self.__init__(self.width, self.height, self.x, self.y)
-                    else:
-                        self.id = value
-                elif key == "width":
-                    self.width = value
-                elif key == "height":
-                    self.height = height
-                elif key == "x":
-                    self.x = value
-                elif key == "y":
-                    self.y = value
+        for key, value in kwargs.items():
+            if key == "id":
+                self.id = value
+            elif key == "width":
+                self.width = value
+            elif key == "height":
+                self.height = height
+            elif key == "x":
+                self.x = value
+            elif key == "y":
+                self.y = value
 
 
 
