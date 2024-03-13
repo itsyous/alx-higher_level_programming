82d4025bfe9c9f1c0c2682114e0f882bd5df8943
itsyous <youssef.rabih1995@gmail.com>
2024-03-13 13:59:00 -0700

    Root user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- create the MySQL server user user_0d_1
 -- and grant all privileges
 CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON * . * user_0d_1@localhost;
+GRANT ALL ON *.* TO user_0d_1@localhost;
