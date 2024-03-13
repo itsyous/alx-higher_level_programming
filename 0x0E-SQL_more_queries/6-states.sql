868193de54cce7def3df2dd3b3ab0876f4f1fcf8
itsyous <youssef.rabih1995@gmail.com>
2024-03-13 16:14:50 -0700

    States table

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,7 +1,4 @@
 -- create the database hbtn_0d_usa and its table
 CREATE DATABASE IF NOT EXISTS 'hbtn_0d_usa';
-CREATE TABLE IF NOT EXISTS 'hbtn_0d_usa'.'states' (
-	PRIMARY KEY('id'),
-	'id'  INT        NOT NULL AUTO_INCREMENT,
-	'name' VARCHAR(256) NOT NULL
-	);
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (id INT UNIQUE NOT NULL AUTO_INCREMENT, name VARCHAR(256) NOT NULL, PRIMARY KEY(id));
