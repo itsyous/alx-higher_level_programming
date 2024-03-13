2403cfb21b2464826544e743f242683fc4957fbc
itsyous <youssef.rabih1995@gmail.com>
2024-03-13 16:11:57 -0700

    Genre ID for all shows

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,6 +1,5 @@
 -- list all shows contained on database hbtn_0d_tvshows
-SELECT s.'title', g.'genre_id'
-  FROM 'tv_shows' AS s
-       LEFT JOIN 'tv_show_genres' AS g
-       ON s.'id' = g.'show_id'
-  ORDER BY s.'title', g.'genre_id';
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
