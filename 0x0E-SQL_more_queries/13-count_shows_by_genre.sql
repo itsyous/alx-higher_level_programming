f5265d1684e92e6939830aba0f8da34b18386d39
itsyous <youssef.rabih1995@gmail.com>
2024-03-13 16:10:13 -0700

    Number of shows by genre

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,8 +1,6 @@
 -- list all genres from hbtn_0d_tvshows along
-SELECT g.'name' AS 'genre',
-       COUNT(*) AS 'number_of_shows'
-FROM 'tv_genres' AS g
-INNER JOIN 'tv_shows_genres' AS t
-ON g.'id' = t.'genre_id'
-GROUP BY g.'name'
-ORDER BY 'number_of_shows' DESC;
+SELECT tv_genres.name AS 'genre', COUNT(tv_show_genres.genre_id) AS 'number_of_shows'
+FROM tv_genres RIGHT JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY genre
+ORDER BY number_of_shows DESC;
