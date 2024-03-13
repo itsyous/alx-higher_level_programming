8a1e66260bbf9fa4f41d3b35d4c4ae841972744d
itsyous <youssef.rabih1995@gmail.com>
2024-03-13 15:59:40 -0700

    No genre

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,6 @@
+-- list all shows contained on hbtn_0d_0_tvshows
+SELECT tv_shows.title, tv_show_genres.genre_id
+    FROM tv_shows LEFT JOIN tv_show_genres
+    ON tv_shows.id = tv_show_genres.show_id
+    WHERE tv_show_genres.genre_id IS NULL
+    ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
