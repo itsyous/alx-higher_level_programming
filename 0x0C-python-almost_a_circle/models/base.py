26883e44d8bb2520d2138e67e06e5575d27a580f
itsyous <youssef.rabih1995@gmail.com>
2024-03-12 17:43:13 -0700

    JSON string to dictionary

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -33,7 +33,7 @@ class Base:
     @staticmethod
     def from_json_string(json_string):
         """convert from json string to dictionary"""
-        if json_string is None:
+        if json_string is None or json_string == "[]":
             return "[]"
         else:
             return json.loads(json_string)
